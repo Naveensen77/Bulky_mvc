3ebb9812d6436d983d5ad2a9a813ae7b4c1c837f
Naveen <113160204+Naveensen77@users.noreply.github.com>
2024-01-18 21:54:33 +0530

    up

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -29,8 +29,60 @@ namespace BulkyWeb.Controllers
                 _db.SaveChanges();
                 return RedirectToAction("Index");
             }
-          return View();
+            return View();
         }
+
+        public IActionResult EditCategory(int? Id)
+        {
+            if (Id == null || Id == 0)
+                return NotFound();
+            Category categoryfromdb = _db.Categorys.Find(Id);
+            //Category categoryfromdb2 = _db.Categorys.FirstOrDefault(u=>u.Id==Id);
+            //Category categoryfromdb3 = _db.Categorys.Where(t=>t.Id==Id).FirstOrDefault;
+            if (categoryfromdb == null)
+                return NotFound();
+            return View(categoryfromdb);
+
+        }
+        [HttpPost]
+        public IActionResult EditCategoryData(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categorys.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult DeleteCategory(int? Id)
+        {
+            if (Id == null || Id == 0)
+                return NotFound();
+            Category categoryfromdb = _db.Categorys.Find(Id);
+            if (categoryfromdb == null)
+                return NotFound();
+            return View(categoryfromdb);
+
+        }
+        public IActionResult DeleteCategoryPost(int? Id)
+        {
+            Category? obj = _db.Categorys.Find(Id);
+
+            if (obj == null)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _db.Categorys.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
     }
 
 }
